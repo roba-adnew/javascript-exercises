f323373b67183ab023fe5ee52596a49a0fb22011
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-13 11:14:30 -0500

    Rounding out javascript exercises 5
    See subject line
    
    	modified:   sumAll.js
    	modified:   sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(oneNum, otherNum) {
 
+    if (typeof(oneNum)  !== "number" || typeof(otherNum)  !== "number" || 
+        oneNum < 0 || otherNum < 0) {
+        return 'ERROR'
+    }
+
+    const minNum = oneNum < otherNum ? oneNum : otherNum;
+    const maxNum = oneNum > otherNum ? oneNum : otherNum;
+
+    let sumNum = 0;
+
+    for (let i = minNum; i <= maxNum; i++) {
+        sumNum += i;
+    }
+
+    return sumNum;
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
