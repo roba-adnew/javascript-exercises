cf1545874b7d31857d810e1895a19ea1f7d9c5a3
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-21 14:12:39 -0400

    Completed exercise  Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   fibonacci.js
    	modified:   fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = typeof(num);
+    if (typeof(num) != 'number' && typeof(parseInt(num)) != 'number') 
+        return "OOPS";
 
+    if (typeof parseInt(num) == 'number') num = parseInt(num);
+    if (num < 1) return "OOPS";
+    if (num == 1 || num == 2) return 1;
+    
+    let fib = [1,1]
+    let i = fib.length;
+    while (i < num) {
+        let next = fib[i-1] + fib[i-2];
+        fib.push(next);
+        i++;
+    }
+
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
