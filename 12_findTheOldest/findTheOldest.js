69060791cb8322687d1ce8a0f174f4491b6e84c8
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-21 15:02:40 -0400

    Completed exercise
     Changes to be committed:
    	modified:   findTheOldest.js
    	modified:   findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const older = people.sort((p1, p2) => {
 
+        const living = new Date().getFullYear();
+
+        const p1YearOfDeath = p1.yearOfDeath ? p1.yearOfDeath : living;
+        const p1Age = p1YearOfDeath - p1.yearOfBirth;
+
+        const p2YearOfDeath = p2.yearOfDeath ? p2.yearOfDeath : living;
+        const p2Age = p2YearOfDeath - p2.yearOfBirth;
+
+        return p1Age > p2Age ? 1 : -1
+    })
+    return older[older.length - 1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.spec.js
+++ 12_findTheOldest/findTheOldest.spec.js
@@ -6,55 +6,55 @@ describe('findTheOldest', () => {
       {
         name: "Carly",
         yearOfBirth: 1942,
-        yearOfDeath: 1970,
+        yearOfDeath: 1970, // 28
       },
       {
         name: "Ray",
         yearOfBirth: 1962,
-        yearOfDeath: 2011,
+        yearOfDeath: 2011, // 49
       },
       {
         name: "Jane",
         yearOfBirth: 1912,
-        yearOfDeath: 1941,
+        yearOfDeath: 1941, // 29
       },
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the person with the greatest age if someone is still living', () => {
+  test('finds the person with the greatest age if someone is still living', () => {
     const people = [
       {
         name: "Carly",
-        yearOfBirth: 2018,
+        yearOfBirth: 2018, //5
       },
       {
         name: "Ray",
         yearOfBirth: 1962,
-        yearOfDeath: 2011,
+        yearOfDeath: 2011, //49
       },
       {
         name: "Jane",
         yearOfBirth: 1912,
-        yearOfDeath: 1941,
+        yearOfDeath: 1941, //29
       },
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the person with the greatest age if the OLDEST is still living', () => {
+  test('finds the person with the greatest age if the OLDEST is still living', () => {
     const people = [
       {
         name: "Carly",
-        yearOfBirth: 1066,
+        yearOfBirth: 1066, //956
       },
       {
         name: "Ray",
         yearOfBirth: 1962,
-        yearOfDeath: 2011,
+        yearOfDeath: 2011, //49
       },
       {
         name: "Jane",
         yearOfBirth: 1912,
-        yearOfDeath: 1941,
+        yearOfDeath: 1941, //29
       },
     ]
     expect(findTheOldest(people).name).toBe('Carly');
