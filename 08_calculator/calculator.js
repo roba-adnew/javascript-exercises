8b1b5b880fd635624c45cba658b65a5aa5f5a3bb
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-21 12:15:41 -0400

    Completed the exercise
     Changes to be committed:
    	modified:   calculator.js
    	modified:   calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {return arr.reduce((cumm,value) => 
+  {return cumm + value},0)
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((product, value) => {
+    return product * value},1);
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+  let fact = 1;
+  while (a > 0) {
+    fact *= a;
+    a -= 1;
+  };
+  return fact;
 };
 
 // Do not edit below this line
