c06768908e71d3121a148d962ac44992ce41762e
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-21 13:22:06 -0400

    Completed the exercise  Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   palindromes.js
    	modified:   palindromes.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (original) {
+    // First I want to filter out all punctuation marks
+    // Then ensure every letter is lower case or check if there is a simple 
+    // way to compare upper and lower case
+    // Then flip the word and check if it's the same as the original
 
+    const origArr = Array.from(original).filter(letter => 
+        letter.match(/^[0-9a-zA-Z]+$/)).
+        map(letter => letter.toLowerCase())
+    
+    const newArr = [...origArr].reverse();
+
+    const lengthMatch = origArr.length === newArr.length;
+
+    let contentMatch = true;
+    for (let i = 0; i < origArr.length; i++) {
+        if (origArr[i] !== newArr[i]) {
+            contentMatch = false;
+            break;
+        } 
+    }
+
+    return lengthMatch && contentMatch;
 };
 
 // Do not edit below this line
